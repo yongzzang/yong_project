a12ad811a9f138acca2e03ea462476994dad699f
yongzzang <lyonggeon@gmail.com>
2021-01-02 14:01:35 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.eventNames.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./Model/User");
+const config = require("./config/key");
+
+//application/x-www-forim-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://yongzzang:dyd112@boilerplate.s1wzi.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+//DB연결
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true, 
@@ -13,7 +22,23 @@ mongoose.connect('mongodb+srv://yongzzang:dyd112@boilerplate.s1wzi.mongodb.net/<
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!, 텍스트 바꾸기 ㅋㅋㄹㅃㅃ')
+})
+
+app.post('/register', (req,res)=> {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 DB에 넣어준다.
+ 
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=> {
+    if(err) return res.json({success: false, error})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
+
 })
 
 app.listen(port, () => {
